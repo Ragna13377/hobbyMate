195e5be76a6bd3c8f54893aaf4fdb773706ec691
Ivan Dmitrievich <quentin.gothic@gmail.com>
2024-10-10 02:26:46 +0300

    feat: add news prisma files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import countries from './seeds/countrySeed.json';
 import hobbies from './seeds/hobbySeed.json';
+import news from './seeds/newsSeed.json';
 
 const prisma = new PrismaClient();
 const countrySeed = async () => {
-	await prisma.country.createMany({ data: countries });
-	await prisma.hobby.createMany({ data: hobbies })
+	await Promise.all([
+		prisma.country.createMany({ data: countries }),
+		prisma.hobby.createMany({ data: hobbies }),
+		prisma.news.createMany({ data: news }),
+	]);
 };
 countrySeed()
 	.catch((e) => console.log(e))
