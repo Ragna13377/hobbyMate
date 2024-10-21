e5d72c2d3abf940d8cb6ba2c3648f726d7814871
Ivan Dmitrievich <quentin.gothic@gmail.com>
2024-10-21 19:16:49 +0300

    hotfix: clear about page

--- src/pages/about/ui/AboutPage.tsx
+++ src/pages/about/ui/AboutPage.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Oauth } from '@features/auth/components/Oauth';
 
 export const AboutPage = () => {
 	return (
 		<div className='p-40 flex flex-col gap-5 text-center'>
 			<div>About</div>
-			<Oauth />
 		</div>
 	);
 };
